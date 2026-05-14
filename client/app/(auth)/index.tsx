120fdb3ab92ac4d30761f01ac1f943faaf7cd9ae
Marc Brolly <marcrobertjohn@gmail.com>
2026-05-14 12:28:04 +0100

    Redesign welcome page, add new background and logo

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -19,16 +19,22 @@ export default function WelcomeScreen() {
   return (
     <ImageBackground
       source={require("../../assets/images/welcomeBG.png")}
+      imageStyle={{}}
       style={styles.background}
       resizeMode="cover"
     >
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require("../../assets/images/logo.png")}
-        />
-        <Text style={[styles.tagline]}>Train with intent.</Text>
-        <View style={{ width: "100%" }}>
+      <View style={styles.overlay}>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require("../../assets/images/logo.png")}
+          />
+
+          <Text style={[styles.tagline]}>Train with intent.</Text>
+        </View>
+        <View
+          style={{ width: "100%", paddingHorizontal: 30, paddingBottom: 50 }}
+        >
           <Pressable
             onPress={() => router.navigate("/(auth)/login")}
             style={styles.buttonContainer}
@@ -50,8 +56,7 @@ export default function WelcomeScreen() {
 const makeStyles = (theme: Theme, scale: number) =>
   StyleSheet.create({
     container: {
-      paddingVertical: 100,
-      paddingHorizontal: 30,
+      paddingTop: 40,
       alignItems: "center",
       flex: 1,
     },
@@ -60,36 +65,43 @@ const makeStyles = (theme: Theme, scale: number) =>
       width: "100%",
       height: "100%",
     },
-
+    overlay: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: "rgba(10, 5, 20, 0.6)",
+    },
+    imageNL: {
+      width: 400,
+      height: 400,
+    },
     image: {
-      width: 300,
-      height: 300,
+      width: 450,
+      height: 450,
     },
     buttonContainer: {
-      backgroundColor: "#E6DAC9",
-      borderColor: theme.border,
-      borderWidth: 1,
-      borderRadius: 12,
-      padding: 12,
+      backgroundColor: "rgba(40, 25, 60, 0.35)",
+      borderColor: "rgba(180, 140, 255, 0.25)",
+      borderWidth: 2,
+      borderRadius: 16,
+      paddingVertical: 12,
+
       width: "100%",
-      marginVertical: 10,
+      marginVertical: 8,
       alignItems: "center",
+
+      // borderColor: "white",
     },
     buttonText: {
-      textAlign: "center",
-      color: "#180C19",
+      color: "#F4EEFF",
       fontWeight: "600",
-      letterSpacing: 0.5,
-      fontSize: 18,
+      letterSpacing: 2,
+      fontSize: 14,
     },
 
     tagline: {
-      fontSize: 24,
-      color: "#E6DAC9",
-      marginBottom: 40,
-      marginTop: -50,
-      textAlign: "center",
-      letterSpacing: 0.5,
+      fontSize: 18,
+      color: "rgba(255,255,255,0.75)",
+      marginTop: -110,
+      letterSpacing: 1,
       fontStyle: "italic",
     },
   });
